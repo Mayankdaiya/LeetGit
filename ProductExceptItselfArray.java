9e8b00099b1d6bca1c7401d97ab51559b9b9da73
Mayank Daiya <mayankdaiya03@gmail.com>
2022-08-31 22:25:29 +0530

    Array

---
+++ ProductExceptItselfArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len=nums.length;
+        int a1[]=new int[len];
+        int p=1;
+        for(int i=0;i<len;i++){
+            a1[i]=p;
+            p*=nums[i];
+        }
+        p=1;
+        for(int i=len-1;i>=0;i--){
+            int t=nums[i];
+            nums[i]=p*a1[i];
+            p*=t;
+        }
+        return nums;
+    }
+}
