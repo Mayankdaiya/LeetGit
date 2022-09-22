5dd6d584105e5a793fe55a3c04625bae579c84d5
Mayank Daiya <mayankdaiya03@gmail.com>
2022-09-22 09:17:29 +0530

    JAVA

---
+++ LinkedList/LL_Cycle.java
@@ -0,0 +1,20 @@
+public class LL_Cycle {
+    public boolean hasCycle(ListNode head) {
+
+        if(head==null)
+            return false;
+
+        ListNode fast,slow;
+        fast = slow = head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+
+    }
+}
