5bee2f770108edc3ff042a1327409c0e7f26f599
Mayank Daiya <mayankdaiya03@gmail.com>
2022-09-25 17:17:50 +0530

    Array

---
+++ Array/House_Robber.java
@@ -0,0 +1,14 @@
+class House_Robber {
+    public int rob(int[] nums) {
+        int prevMax = 0;
+        int currentMax = 0;
+
+        for(int num : nums) {
+            int sum = prevMax + num;
+            prevMax = currentMax;
+            currentMax = Math.max(sum, currentMax);
+        }
+        return currentMax;
+
+    }
+}
