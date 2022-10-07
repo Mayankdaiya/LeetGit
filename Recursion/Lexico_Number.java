2f5f4b32800c02fdacf43055f128ef86c6a4c29c
Mayank Daiya <mayankdaiya03@gmail.com>
2022-10-07 18:35:06 +0530

    Recursion

--- Recursion/Lexico_Number.java
+++ Recursion/Lexico_Number.java
@@ -1,4 +1,5 @@
 class Lexico_Number {
+    
     public static void print_lexico_num(int num,int n,ArrayList<Integer> list){
         if(num>n) return;
         if(num!=0) list.add(num);
@@ -6,7 +7,6 @@ class Lexico_Number {
         if(num==0) i=1;
         for(;i<=9;i++) print_lexico_num(num*10+i,n,list);
     }
-
     public List<Integer> lexicalOrder(int n) {
         ArrayList<Integer> list=new ArrayList<>();
         print_lexico_num(0,n,list);
