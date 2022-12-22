b8479591bf80249ebd34fe4e5bcb7e2085884859
Mayankdaiya <mayankdaiya03@gmail.com>
2022-12-22 13:50:15 +0530

    Stack

---
+++ Stack/MinStack.java
@@ -0,0 +1,30 @@
+class MinStack {
+    Stack<Integer> st=new Stack<>();
+    Stack<Integer> min=new Stack<>();
+    public MinStack() {
+
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        if(min.isEmpty()) min.push(val);
+        else{
+            if(st.peek()<=min.peek()) min.push(st.peek());
+        }
+    }
+    
+    public void pop() {
+        if(st.peek()==min.peek()) min.pop();
+        st.pop();
+        if(st.isEmpty()) min.pop();
+    }
+    
+    public int top() {
+       return st.peek();
+    }
+    
+    public int getMin() {
+        
+        return min.peek();
+    }
+}
