e185067c6497a50f80794cf7e161eac6acdc1e03
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-11 19:07:58 +0530

    Problem no. 1910

---
+++ String/substring_remove.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        return removeSubstring(s,part);
+    }
+    public static String removeSubstring(String s1,String s2){
+        for(int i=0;i<=(s1.length()-s2.length());i++){
+            String str=s1.substring(i);
+            if(str.startsWith(s2)){
+                s1=s1.substring(0,i)+s1.substring(i+s2.length());
+                i=-1;
+            }
+        }
+        return s1;
+    }
+}
