b8aaf1540f9d700b8c753883d216179cadd12029
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-13 21:16:40 +0530

    2405

---
+++ String/optimal_string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int partitionString(String s) {
+        int arr[]=new int[26];
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            if(arr[s.charAt(i)-'a']==1){
+                c++;
+                Arrays.fill(arr,0);
+                arr[s.charAt(i)-'a']++;
+            }
+            else arr[s.charAt(i)-'a']++;
+        }
+        return c+1;
+    }
+}
