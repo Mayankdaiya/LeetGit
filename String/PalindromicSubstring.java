4b4ab1b1abd1cf615573c64035c4da53168ac828
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-15 22:30:34 +0530

    Problem 647

--- String/PalindromicSubstring.java
+++ String/PalindromicSubstring.java
@@ -2,6 +2,7 @@ public class Solution {
     public int countSubstrings(String s) {
         int ans = 0;
         for(int i = 0; i < s.length() ; i++){
+            //for odd length
             int left = i;
             int right = i;
             while(left >= 0 && right < s.length()){
@@ -14,6 +15,7 @@ public class Solution {
                     break;
                 }
             }
+            //for even length
             left = i;
             right = i + 1;;
             while(left >= 0 && right < s.length()){
