3ff9f46ad519e4080d5181f151f289fc43aec995
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-17 20:48:45 +0530

    Problem 1005

---
+++ Array/MaximizeSubarray.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(k==0) break;
+            if(nums[i]<=0){
+                nums[i]*=-1;
+                if(nums[i]==0) i--;
+                k--;
+            } 
+            else break;
+        }
+        Arrays.sort(nums);
+        int s=0, in=0;
+        while(k>0){
+            nums[in]*=-1;
+            k--;
+            // in++
+        }
+        for(int i=0;i<nums.length;i++) s+=nums[i];
+        return s;
+    }
+} 
