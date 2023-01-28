e28031eb5739f7eb294d75c78026483df234db46
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-28 21:09:28 +0530

    Problem 589

--- Tree/PreorderGeneralTree.java
+++ Tree/PreorderGeneralTree.java
@@ -1,25 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
 
 class Solution {
+    
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
     public List<Integer> preorder(Node root) {
         List<Integer> list=new ArrayList<>();
         Preorder(root, list);
