1ba2a648c2793ad07849e124c8f3eed33582c493
Mayankdaiya <mayankdaiya03@gmail.com>
2023-01-28 21:07:29 +0530

    s

---
+++ Tree/PreorderGeneralTree.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list=new ArrayList<>();
+        Preorder(root, list);
+        return list;
+    }
+    public static void Preorder(Node root,List<Integer> list){
+        if(root==null) return;
+        list.add(root.val);
+        for(Node i:root.children){
+            Preorder(i,list);
+        }
+    }
+}
