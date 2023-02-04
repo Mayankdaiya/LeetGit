497769d85494b1fcb671e1182e3d83f47c6bc786
Mayankdaiya <mayankdaiya03@gmail.com>
2023-02-04 22:14:13 +0530

    k

---
+++ Array/SeparateDigits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int t=nums[i];
+            int p=(int)Math.pow(10,(int)Math.log10(t));
+            while(p>0){
+                int tmp=(t/p)%10;
+                list.add(tmp);
+                p/=10;
+            }
+        }
+        int ans[]=new int[list.size()];
+        for(int i=0;i<list.size();i++) ans[i]=list.get(i);
+        return ans;
+    }
+}
