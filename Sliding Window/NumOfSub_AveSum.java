188d4545fec5cc5f2e95c9b9895e962904d8519a
Mayankdaiya <mayankdaiya03@gmail.com>
2023-04-05 14:00:42 +0530

    Problem 1343

---
+++ Sliding Window/NumOfSub_AveSum.java
@@ -0,0 +1,14 @@
+public class NumOfSub_AveSum {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int res=0, av=0;
+        for(int i=0;i<k;i++){
+            av+=arr[i];
+        }
+        if(av/k>=threshold) res++;
+        for(int i=k;i<arr.length;i++){
+            av=av-arr[i-k]+arr[i];
+            if(av/k>=threshold) res++;
+        }
+        return res;
+    }
+}
