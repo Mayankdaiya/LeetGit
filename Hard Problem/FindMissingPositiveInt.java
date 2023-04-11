9e44f42412093fb36e2cd3a1ac82994c5fe5f000
Mayankdaiya <mayankdaiya03@gmail.com>
2023-04-11 14:26:46 +0530

    Problem-hard 41

--- Hard Problem/FindMissingPositiveInt.java
+++ Hard Problem/FindMissingPositiveInt.java
@@ -16,4 +16,21 @@ public class FindMissingPositiveInt {
        }
       return nums.length+1;
     }
+    public int firstMissingPositive_optimal(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int Indexed_pos=nums[i]-1;
+            if(nums[i]>0 && nums[i]<=nums.length && nums[i]!=nums[Indexed_pos]){
+                int temp=nums[i];
+                nums[i]=nums[Indexed_pos];
+                nums[Indexed_pos]=temp;
+            } else{
+                i++;
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1) return j+1;
+        }
+        return nums.length+1;
+     }
 }
